91af84386752f7b826821f69dbf0fc82883ca727
jcapissolato <jcapissolato@gmail.com>
2022-02-15 17:06:39 -0300

    exercicios de operadores feitos

---
+++ modulo1/operadores/index.js
@@ -0,0 +1,99 @@
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
+
+// /*
+// const bool1 = true 
+// const bool2 = false
+// const bool3 = !bool2 = true
+// */
+
+// let resultado = bool1 && bool2
+// console.log("a. ", resultado)
+
+// /*console.log("a.", false)*/
+
+// resultado = bool1 && bool2 && bool3 
+// console.log("b. ", resultado)
+
+// /*console.log("b.", false) */
+
+// resultado = !resultado && (bool1 || bool2) 
+// console.log("c. ", resultado)
+
+// /*resultado = false && true 
+// console.log("c.", false) */
+
+// console.log("d. ", typeof resultado)
+
+/*console.log("d.", typeof booleano) */
+
+/* Resolução do exercício 3*/
+
+
+// let primeiroNumero = prompt("Digite um numero!")
+// let segundoNumero = prompt("Digite outro numero!")
+// let numero1 = Number(primeiroNumero)
+// let numero2 = Number(segundoNumero)
+
+// const soma = numero1 + numero2
+
+// console.log(soma)
+
+// let idadeDoUsuario = prompt("digite sua idade")
+// let idadeDoMelhorAmigo = prompt("digite a idade do seu melhor ammigo")
+// let usuario = Number(idadeDoUsuario)
+// let melhorAmigo = Number(idadeDoMelhorAmigo)
+// const idadeMaior = usuario > melhorAmigo
+
+// console.log("Sua idade é maior do que seu melhor amigAo?",idadeMaior )
+
+// const diferencaDeIdade = melhorAmigo - usuario
+
+// console.log("A difença de idade é:", diferencaDeIdade)
+
+// const numeroPar = prompt("Digite um número par:")
+// const Par = Number(numeroPar)
+
+// console.log("o resto da divisão por dois é:", Par % 2)
+
+//Todos terminam em zero, pois todo numero par é divisivel por 2
+
+// Inserindo número impar aparece o resto da divisão
+
+// const idadeDoUsuario = prompt("digite aqui sua idade")
+// const idade = Number(idadeDoUsuario)
+// const idadeEmMeses = idade * 12
+// const idadeEmDias = idadeEmMeses * 365
+// const idadeEmHoras =idadeEmDias * 24
+
+
+// console.log("idade do usuário em meses", idadeEmMeses)
+// console.log("idade do usuario em dias", idadeEmDias )
+// console.log("idade do usuario em horas", idadeEmHoras)
+
+
+// let digiteUmNumero = prompt("digite um numero aqui")
+// let digiteOutroNumero = prompt("digite outro numero aqui")
+// let primeiroNumero = Number(digiteUmNumero)
+// let segundoNumero = Number(digiteOutroNumero)
+// let primeiroMaiorQueSegundo = primeiroNumero > segundoNumero
+// let primeiroIgualSegundo = primeiroNumero >= segundoNumero
+// let primeiroDivisivel = primeiroNumero / segundoNumero
+// let segundoDivisivel = segundoNumero / primeiroNumero
+
+// console.log("primeiro numero é maior que o segundo?", primeiroMaiorQueSegundo)
+// console.log("Primeiro numero é igual ao segundo?", primeiroIgualSegundo)
+// console.log("primeiro numero é divisivel pelo segundo?", primeiroDivisivel === 0)
+// console.log("o segundo numero é divisivel pelo primeiro?", segundoDivisivel === 0)
+
+
+
+
+
+
+
+
+
+
+
