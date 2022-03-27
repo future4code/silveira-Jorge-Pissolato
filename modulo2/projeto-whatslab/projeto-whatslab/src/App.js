2392dc40edcd52875a257a44ef2a93bed376dc48
jcapissolato <jcapissolato@gmail.com>
2022-03-27 17:19:27 -0300

    projeto-whatslab terminado

--- modulo2/projeto-whatslab/projeto-whatslab/src/App.js
+++ modulo2/projeto-whatslab/projeto-whatslab/src/App.js
@@ -1,42 +1,107 @@
 import React from "react"
+import styled from "styled-components"
 
-class MeuComponenteReact extends React.Component {
+
+
+const InputNome = styled.input`
+ width: 100px;
+
+`
+
+const InputMensagem = styled.input`
+ flex-grow: 1;
+
+`
+
+const Inputs = styled.div`
+
+
+
+display: flex;
+height: 4vh;
+background-color: azure;
+width: 500px; 
+margin-left: auto;
+margin-right: auto;
+
+`
+
+const ContainerMensagem = styled.div`
+	
+    height: 100vh;
+    border: 1px solid black;
+    flex: 1 1 0%;
+    display: flex;
+    flex-direction: column-reverse;
+	background-color: azure;
+	width: 500px; 
+    margin-left: auto;
+    margin-right: auto;
+	
+   
+`
+
+const Container = styled.div`
+height: 100%;
+	
+`
+
+
+
+
+class App extends React.Component {
 	state = {
 		nome: '',
-    mensagem: ''
+		mensagem: '',
+		listaDeMensagens: []
 	}
 
 	onChangeNome = (event) => {
-		this.setState({nome: event.target.value})
-   
+		this.setState({ nome: event.target.value })
+
 	}
 
-  onChangeMensagem = (event) => {
-		this.setState({mensagem: event.target.value})
+	onChangeMensagem = (event) => {
+		this.setState({ mensagem: event.target.value })
 	}
 
-  onClickEnviar = () => {
-    console.log(this.state)
-    this.setState({mensagem: ''})
-  }
+	onClickEnviar = () => {
+		let novaMensagem = { nome: this.state.nome, mensagem: this.state.mensagem }
+		let novaLista = [novaMensagem, ...this.state.listaDeMensagens]
+		this.setState({ listaDeMensagens: novaLista })
+		this.setState({ nome: '', mensagem: '' })
+	}
+
+	enviarComEnter = (event) => {
+		console.log(event.key)
+		if (event.key === 'Enter') {
+			this.onClickEnviar()
+		}
+	}
 
 	render() {
-    console.log(this.state)
-    
 		return (
-        <div>
-				  <input placeholder={"Usuário"} value={this.state.nome} onChange={this.onChangeNome}/>
-				  <input placeholder={"Mensagem"} value={this.state.mensagem} onChange={this.onChangeMensagem}/>
-          <div>
-            <p><b>{this.state.nome}</b>  {this.state.mensagem}</p>
-          </div>
-            <button onClick={this.onClickEnviar}>Enviar</button>
-        </div>
+			<div>
+				<ContainerMensagem>
+					{this.state.listaDeMensagens.map((item, index) => {
+						return <p><b>{item.nome}:</b> {item.mensagem}</p>
+					})}
+				</ContainerMensagem>
+				<Container>
+					<Inputs>
+						<InputNome placeholder={"Usuário"} value={this.state.nome} onChange={this.onChangeNome} />
+						<InputMensagem placeholder={"Mensagem"} value={this.state.mensagem} onChange={this.onChangeMensagem}
+							onKeyPress={this.enviarComEnter} />
+
+						<button onClick={this.onClickEnviar}>Enviar</button>
+					</Inputs>
+				</Container>
+			</div>
 
 		)
 	}
 }
 
-export default MeuComponenteReact
+export default App
 
 
