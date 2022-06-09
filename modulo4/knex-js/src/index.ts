deffdd76f648e57e2f18ce4d549a45d3fc400b36
jcapissolato <jcapissolato@gmail.com>
2022-06-09 20:18:21 -0300

    knex-js

--- modulo4/knex-js/src/index.ts
+++ modulo4/knex-js/src/index.ts
@@ -4,7 +4,8 @@ import cors from "cors";
 //extra: importando configuração de rede do node
 import { AddressInfo } from "net";
 import { Knex } from "knex";
-
+import connection from "./connection";
+import { request } from "https";
 
 
 //iniciando a aplicação web com express
@@ -16,7 +17,34 @@ app.use(cors());
 
 //Exemplos - consultas no banco ou querys
 //Exemplos - endpoints
+app.get("/actor", async(req:Request, res:Response)=>{
+  try{
+    const result = await connection.raw(`
+    SELECT * FROM Actor
+    `)
+    res.status(200).send(result[0])
+  }catch(error){
+    res.status(400).send({error})
+  }
+})
 
+app.post("/actor", async(req:Request, res:Response)=>{
+  try{
+     await connection.raw(`
+    INSERT INTO Actor (id, name, salary, birth_date, gender)
+    VALUES(
+      "${Date.now().toString()}", 
+      "${req.body.name}, 
+      ${req.body.salary} 
+      "${req.body.birthDate}", 
+      "${req.body.gender}"
+      );
+    `)
+    res.status(201).send("Ator criado com sucesso")
+  }catch(error: any){
+    res.status(500).send(error.sqlMessage || error.message )
+  }
+})
 //Exercício 1
 
 //a) Explique como é a resposta que temos quando usamos o raw. 
@@ -29,23 +57,23 @@ app.use(cors());
 
 // b) Faça uma função que busque um ator pelo nome;
 
-const buscarActor = async (name: string): Promise<any> => {
-  const result = await connection.raw(`
-    SELECT * FROM Actor WHERE name = "${name}"
-  `)
-  return result
-}
+// const buscarActor = async (name: string): Promise<any> => {
+//   const result = await connection.raw(`
+//     SELECT * FROM Actor WHERE name = "${name}"
+//   `)
+//   return result
+// }
 
 // c) Faça uma função que receba um gender retorne a quantidade de itens na tabela Actor com esse gender. 
 // Para atrizes, female e para atores male.
 
-const contaActors = async (gender: string): Promise<any> => {
-  const result = await connection.raw(`
-    SELECT COUNT(*) as count FROM Actor WHERE gender = "${gender}"
-  `);
-  const conta = result[0][0].conta;
-  return conta;
-};
+// const contaActors = async (gender: string): Promise<any> => {
+//   const result = await connection.raw(`
+//     SELECT COUNT(*) as count FROM Actor WHERE gender = "${gender}"
+//   `);
+//   const conta = result[0][0].conta;
+//   return conta;
+// };
 
 //Servidor
 const server = app.listen(process.env.PORT || 3003, () => {
