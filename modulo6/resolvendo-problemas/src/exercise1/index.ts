137103dd3613f92cbe1b2cf68c62f3141f87a68f
jcapissolato <jcapissolato@gmail.com>
2022-07-25 20:44:28 -0300

    resolvendo-problemas

---
+++ modulo6/resolvendo-problemas/src/exercise1/index.ts
@@ -0,0 +1,7 @@
+// Faça um programa que receba uma string e determine se ela é um palíndromo
+
+// Exemplos:
+// Bob.
+// A dama admirou o rim da amada.
+// Aí, Lima falou: “Olá, família”.
+
---
+++ modulo6/resolvendo-problemas/src/exercise2/index.ts
@@ -0,0 +1,10 @@
+/*
+ DESAFIO: Dado uma tabela de competidores, obert a quantidade total medalhas de cada países
+
+                                competidores
+nome                    federação               modalidade              qdMedalhas
+Usain Bolt              Jamaica                 Atletismo               54
+Rebeca Andrade          Brasil                  Ginástica               33
+Michael Phelps          EUA                     Natação                 71              
+Alison dos Santos       Brasil                  Atletismo               19
+*/
